99d117a9db427076cf8e2d2cb4152b677157ae7e
calvin <cavidasham@gmail.com>
2019-12-19 09:09:49 +0800

    add more in main function

--- src/main/java/org/example/rxjava/App.java
+++ src/main/java/org/example/rxjava/App.java
@@ -1,10 +1,25 @@
 package org.example.rxjava;
 
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Function;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
     public static void main (String args[]){
 
         System.out.println("asd");
+        List<String> someWords = new ArrayList<String>();
+        someWords.add("Word1");
+        someWords.add("Word2");
 
+
+        Observable.fromIterable(someWords)
+                .groupBy(word -> word.equals(word))
+                .subscribe(grouplist ->
+                        System.out.println(grouplist.forEach(System.out::println)));
     }
 }
